a9b5b2073e577f1aa147d87d7eb5d408b0aa5d38
Adam <adamgryu@gmail.com>
2015-01-10 13:14:45 -0500

    added a test scene and some birds

---
+++ Assets/Scripts/BirdSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BirdSpawner : MonoBehaviour {
+	
+	public GameObject birdPrefab;
+	public List<GameObject> spawnPoints;
+	public int timeoutPeriod = 1;
+
+	float timer;
+
+	// Use this for initialization
+	void Start () {
+		this.timer = this.timeoutPeriod;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.timer -= Time.deltaTime;
+		Debug.Log(this.timer);
+		if (this.timer <= 0) {
+			this.timer = this.timeoutPeriod;
+			GameObject newBird = (GameObject)GameObject.Instantiate(birdPrefab);
+			newBird.transform.position = this.spawnPoints[Random.Range(0, this.spawnPoints.Count)].transform.position;
+		}
+	}
+}
